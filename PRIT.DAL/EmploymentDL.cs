2f336481733514500d74043e589acea5a1ba8123
IT09-229\Neosoft <gaurav.raut@neosofttech.com>
2018-04-23 17:33:17 +0530

    initial commit

---
+++ PRIT.DAL/EmploymentDL.cs
@@ -0,0 +1,36 @@
+﻿using PRIT.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIT.DAL
+{
+ public   class EmploymentDL
+    {
+        PRITEntities db = new PRITEntities();
+        public void SaveEmployment(tbl_EmploymentDetails obj)
+        {
+            
+            try
+            {
+                if (obj.ID > 0)
+                {
+                    var entity = db.tbl_EmploymentDetails.Where(p => p.ID == obj.ID).AsQueryable().FirstOrDefault();
+                    db.Entry(entity).CurrentValues.SetValues(obj);
+                }
+                else
+                {
+                    db.tbl_EmploymentDetails.Add(obj);
+                }
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
